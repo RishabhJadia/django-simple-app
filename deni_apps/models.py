49b6cff89f3c35c85c480c80ac066f7d98640b18
Rishabh Jadia <buchav.jadia95@gmail.com>
2023-06-15 01:13:20 +0530

    4th commit

--- deni_apps/models.py
+++ deni_apps/models.py
@@ -31,3 +31,54 @@ class CandidateDetails(Nameable):
 
     class Meta:
         verbose_name = 'Candidate Details'
+
+
+# from django.core.mail import send_mail
+# from django.conf import settings
+# from django.db import models
+
+# class MyModel(models.Model):
+#     name = models.CharField(max_length=100)
+#     age = models.IntegerField()
+#     email = models.EmailField()
+
+#     def save(self, *args, **kwargs):
+#         """
+#         Override the save method to send an email when the age field is set to 30 and saved.
+#         """
+#         if self.pk is None and self.age == 30: # Check if the instance is being created and the age field is 30
+#             subject = 'Age of {} is now 30!'.format(self.name)
+#             message = 'The age of {} ({}) is now 30!'.format(self.name, self.email)
+#             from_email = settings.DEFAULT_FROM_EMAIL
+#             recipient_list = ['recipient@example.com']
+#             send_mail(subject, message, from_email, recipient_list)
+
+#         super().save(*args, **kwargs)
+
+# # In your view, when the user clicks the save button:
+# my_model_instance = MyModel(name='John Doe', age=30, email='example@example.com')
+# my_model_instance.save()
+
+
+
+# from django.core.mail import send_mail
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from django.conf import settings
+# from .models import MyModel
+
+# @receiver(post_save, sender=MyModel)
+# def send_email_on_save(sender, instance, **kwargs):
+#     """
+#     Signal receiver function to send an email when the age field is set to 30 and saved.
+#     """
+#     if instance.age == 30:
+#         subject = 'Age of {} is now 30!'.format(instance.name)
+#         message = 'The age of {} ({}) is now 30!'.format(instance.name, instance.email)
+#         from_email = settings.DEFAULT_FROM_EMAIL
+#         recipient_list = ['recipient@example.com']
+#         send_mail(subject, message, from_email, recipient_list)
+
+# # In your view, when the user clicks the save button:
+# my_model_instance = MyModel(name='John Doe', age=30, email='example@example.com')
+# my_model_instance.save()
